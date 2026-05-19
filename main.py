0ae800edfb1d7f30260a339f192486b2c708dbee
HSINGH5911 <harshcv16@gmail.com>
2026-05-19 14:14:58 -0500

    init commit

---
+++ main.py
@@ -0,0 +1,62 @@
+from matplotlib import pyplot as plt
+
+import fastf1
+import fastf1.plotting
+
+session = fastf1.get_session(2022, "United States Grand Prix", "R")
+session.load()
+laps = session.laps
+
+drivers = session.drivers
+print(drivers)
+
+drivers = [session.get_driver(driver)["Abbreviation"] for driver in drivers]
+print(drivers)
+
+stints = laps[["Driver", "Stint", "Compound", "LapNumber"]]
+stints = stints.groupby(["Driver", "Stint", "Compound"])
+stints = stints.count().reset_index()
+
+stints = stints.rename(columns={"LapNumber": "StintLength"})
+print(stints)
+
+fig, ax = plt.subplots(figsize=(5, 10))
+
+for driver in drivers:
+    driver_stints = stints.loc[stints["Driver"] == driver]
+
+    previous_stint_end = 0
+    for idx, row in driver_stints.iterrows():
+        compound_colors = {
+            "SOFT": "#FF6961",
+            "MEDIUM": "#FDFD96",
+            "HARD": "#FFFFFF",
+            "INTERMEDIATE": "green",
+            "WET": "blue"
+        }
+
+        compound_color = compound_colors[row["Compound"]]
+
+        plt.barh(
+            y=driver,
+            width=row["StintLength"],
+            left=previous_stint_end,
+            color=compound_color,
+            edgecolor="black",
+            fill=True
+        )
+
+        previous_stint_end += row["StintLength"]
+
+plt.title("2022 Hungarian Grand Prix Strategies")
+plt.xlabel("Lap Number")
+plt.grid(False)
+
+ax.invert_yaxis()
+
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['left'].set_visible(False)
+
+plt.tight_layout()
+plt.show()
