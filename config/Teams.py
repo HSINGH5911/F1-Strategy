10fc30bf9bf23bd7becd237c65a1a17b4898a904
HSINGH5911 <harshcv16@gmail.com>
2026-05-23 17:28:53 -1000

    added all team data. Done with commit

---
+++ config/Teams.py
@@ -0,0 +1,122 @@
+TEAMS = {
+    "McLaren": {
+        "base_pace": -0.72,
+        "qualifying_pace": -0.78,
+        "race_pace": -0.74,
+        "pit_stop_avg": 2.15,
+        "reliability": 0.97,
+        "tire_management": 1.12,
+        "drs_efficiency": 1.08,
+        "strategy_rating": 0.94
+    },
+
+    "Red Bull": {
+        "base_pace": -0.65,
+        "qualifying_pace": -0.82,
+        "race_pace": -0.60,
+        "pit_stop_avg": 2.05,
+        "reliability": 0.96,
+        "tire_management": 1.14,
+        "drs_efficiency": 1.12,
+        "strategy_rating": 0.91
+    },
+
+    "Ferrari": {
+        "base_pace": -0.48,
+        "qualifying_pace": -0.62,
+        "race_pace": -0.42,
+        "pit_stop_avg": 2.32,
+        "reliability": 0.94,
+        "tire_management": 0.96,
+        "drs_efficiency": 1.02,
+        "strategy_rating": 0.80
+    },
+
+    "Mercedes": {
+        "base_pace": -0.35,
+        "qualifying_pace": -0.30,
+        "race_pace": -0.38,
+        "pit_stop_avg": 2.45,
+        "reliability": 0.96,
+        "tire_management": 1.03,
+        "drs_efficiency": 1.00,
+        "strategy_rating": 0.86
+    },
+
+    "Aston Martin": {
+        "base_pace": 0.10,
+        "qualifying_pace": 0.05,
+        "race_pace": 0.12,
+        "pit_stop_avg": 2.60,
+        "reliability": 0.92,
+        "tire_management": 0.98,
+        "drs_efficiency": 0.97,
+        "strategy_rating": 0.78
+    },
+
+    "Alpine": {
+        "base_pace": 0.22,
+        "qualifying_pace": 0.18,
+        "race_pace": 0.25,
+        "pit_stop_avg": 2.58,
+        "reliability": 0.90,
+        "tire_management": 0.95,
+        "drs_efficiency": 0.96,
+        "strategy_rating": 0.72
+    },
+
+    "RB": {
+        "base_pace": 0.28,
+        "qualifying_pace": 0.20,
+        "race_pace": 0.32,
+        "pit_stop_avg": 2.42,
+        "reliability": 0.93,
+        "tire_management": 1.01,
+        "drs_efficiency": 1.00,
+        "strategy_rating": 0.79
+    },
+
+    "Williams": {
+        "base_pace": 0.35,
+        "qualifying_pace": 0.26,
+        "race_pace": 0.42,
+        "pit_stop_avg": 2.50,
+        "reliability": 0.91,
+        "tire_management": 0.93,
+        "drs_efficiency": 1.06,
+        "strategy_rating": 0.74
+    },
+
+    "Kick Sauber": {
+        "base_pace": 0.52,
+        "qualifying_pace": 0.48,
+        "race_pace": 0.56,
+        "pit_stop_avg": 2.70,
+        "reliability": 0.88,
+        "tire_management": 0.90,
+        "drs_efficiency": 0.94,
+        "strategy_rating": 0.68
+    },
+
+    "Haas": {
+        "base_pace": 0.58,
+        "qualifying_pace": 0.50,
+        "race_pace": 0.65,
+        "pit_stop_avg": 2.55,
+        "reliability": 0.89,
+        "tire_management": 0.87,
+        "drs_efficiency": 0.98,
+        "strategy_rating": 0.70
+    },
+
+    "Cadillac": {
+        "base_pace": 0.75,
+        "qualifying_pace": 0.68,
+        "race_pace": 0.82,
+        "pit_stop_avg": 2.80,
+        "reliability": 0.84,
+        "tire_management": 0.88,
+        "drs_efficiency": 0.92,
+        "strategy_rating": 0.66
+    }
+}
