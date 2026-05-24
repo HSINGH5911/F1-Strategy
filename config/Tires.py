4d766cfadda592f521c03e6af2464a40a1038dd3
HSINGH5911 <harshcv16@gmail.com>
2026-05-23 17:24:42 -1000

    added all data for the tyres

---
+++ config/Tires.py
@@ -0,0 +1,54 @@
+TIRES = {
+    "SOFT": {
+        "base_pace": -0.8,
+        "wear_rate": 0.008,
+        "max_distance": 120,
+        "optimal_temp": 100,
+        "temp_sensitivity": 1.2,
+        "grip_peak": 0.95,
+        "drs_bonus": 0.15,
+        "wet_performance": 0.05
+    },
+
+    "MEDIUM": {
+        "base_pace": 0.0,
+        "wear_rate": 0.005,
+        "max_distance": 180,
+        "optimal_temp": 95,
+        "temp_sensitivity": 1.0,
+        "grip_peak": 0.85,
+        "drs_bonus": 0.10,
+        "wet_performance": 0.08
+    },
+
+    "HARD": {
+        "base_pace": 0.6,
+        "wear_rate": 0.003,
+        "max_distance": 250,
+        "optimal_temp": 90,
+        "temp_sensitivity": 0.8,
+        "grip_peak": 0.75,
+        "drs_bonus": 0.05,
+        "wet_performance": 0.10
+    },
+
+    "INTERMEDIATE": {
+        "base_pace": 6.0,
+        "wear_rate": 0.010,
+        "max_distance": 160,
+        "optimal_temp": 70,
+        "temp_sensitivity": 1.4,
+        "grip_peak": 0.80,
+        "wet_performance": 0.75
+    },
+
+    "WET": {
+        "base_pace": 10.0,
+        "wear_rate": 0.012,
+        "max_distance": 140,
+        "optimal_temp": 60,
+        "temp_sensitivity": 1.5,
+        "grip_peak": 0.70,
+        "wet_performance": 1.0
+    }
+}
